48f1aaadadca91e5d5610408b6942efc9d5a7dae
Viranchee Lotia <viranchee@outlook.com>
2023-06-20 21:22:15 -0700

    Panic handler, Start function

--- rustOS/src/main.rs
+++ rustOS/src/main.rs
@@ -1,5 +1,17 @@
-#![no_std]
+#![no_std] // No linking standard library
+#![no_main] // Disable Rust entry points
 
-fn main() {
-    
+use core::panic::PanicInfo;
+
+// Called on Panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
+}
+
+// Don't change name symbol of this function
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // Entry point. Linker looks for _start()
+    loop{}
 }
