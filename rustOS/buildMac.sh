545cedc9b199f6f5f60e5bac3d774101f0ca774e
Viranchee Lotia <viranchee@outlook.com>
2023-06-25 18:50:04 -0700

    2. Minimal Rust Kernel (#1)
    * Compile OS
    
    * VGA Buffer + Bootloader add
    
    * Minimal Kernel

--- rustOS/buildMac.sh
+++ rustOS/buildMac.sh
@@ -1,1 +1,13 @@
-cargo rustc -- -C link-args="-e __start -static -nostartfiles"
+echo "What do you want to do:"
+echo "1. Build"
+echo "2. Run target on QEMU"
+
+read value
+echo $value
+if [[ $value -eq 1 ]]; then
+  cargo rustc -- -C link-args="-e __start -static -nostartfiles"
+fi
+if [[ $value -eq 2 ]]; then
+  # qemu-system-x86_64 -drive format=raw,file=target/x86-64_rustOS/debug/bootimage-rust_os.bin
+  cargo run
+fi
