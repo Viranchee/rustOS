3b66fdf8e0654668ec685404dee31702f5f5ffaa
Viranchee Lotia <viranchee@outlook.com>
2023-06-25 21:07:34 -0700

    Printing Macros

--- rustOS/src/main.rs
+++ rustOS/src/main.rs
@@ -2,10 +2,12 @@
 #![no_main] // Disable Rust entry points
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 // Called on Panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop{}
 }
 
@@ -14,9 +16,7 @@ static HELLO: &[u8] = b"Hello World!";
 // Don't change name symbol of this function
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
-    
+    println!("Hello World{}", 3.33);
+    panic!("NOPE> DONT CALL THIS");
     loop{}
 }
-
-mod vga_buffer;
