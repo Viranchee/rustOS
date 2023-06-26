7513e9a3f967e8832b5e35fca4173b3719d1d4e1
Viranchee Lotia <viranchee@outlook.com>
2023-06-25 21:08:30 -0700

    3. VGA Text Mode (#2)
    * Volatile Printing
    
    * Printing Macros

--- rustOS/src/main.rs
+++ rustOS/src/main.rs
@@ -2,10 +2,12 @@
 #![no_main] // Disable Rust entry points
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 // Called on Panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop{}
 }
 
@@ -14,15 +16,7 @@ static HELLO: &[u8] = b"Hello World!";
 // Don't change name symbol of this function
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // Entry point. Linker looks for _start()
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    
+    println!("Hello World{}", 3.33);
+    panic!("NOPE> DONT CALL THIS");
     loop{}
 }
