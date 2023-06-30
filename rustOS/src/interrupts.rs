dccec9aaa6de2f89c8dcb22252bc5d0add1af831
Viranchee Lotia <viranchee@outlook.com>
2023-06-30 06:34:18 -0700

    Double Fault Handling

--- rustOS/src/interrupts.rs
+++ rustOS/src/interrupts.rs
@@ -6,6 +6,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     };
 }
@@ -21,4 +22,11 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
 #[test_case]
 fn test_breakpoint_exception() {
     x86_64::instructions::interrupts::int3();
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
--- rustOS/src/main.rs
+++ rustOS/src/main.rs
@@ -9,9 +9,14 @@ use rust_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    rust_os::init();
     println!("Hello World{}", "!");
-    x86_64::instructions::interrupts::int3();
+    rust_os::init();
+
+    // trigger page fault
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 42;
+    }
+
     #[cfg(test)]
     test_main();
 
